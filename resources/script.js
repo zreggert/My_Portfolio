c8f5a0e183c22c17e0ae2eac98776c45efee6f4d
elgalloloco <zreggert@gmail.com>
2021-04-09 15:45:48 -0400

    added functionality to the mobile nav buttons

--- resources/script.js
+++ resources/script.js
@@ -1,25 +1,57 @@
-function showSection(navButton) {
-    console.log(navButton);
-    let sections = $('section').toArray();
-    $.each(sections, function() {
-        let sectionId = $(this).attr('id');
-        if (navButton == sectionId ) {
-            $(this).show("slide", 800);
-        }
-    }) 
-}
+$(document). ready(function() {
 
-$('.nav-btn').click(function() {
-    let navButton = $(this).attr('name');
-    let sections = $('section').toArray();
-    $.each(sections, function() {
-        let sectionId = $(this).attr('id');
-        if (navButton != sectionId) {
-            $(this).hide("blind", 600);
-        }
-    }) 
-   
-    setTimeout(function() {
-        showSection(navButton);
-    }, 700);
+    // function for the nav buttons
+    function showSection(navButton) {
+        console.log(navButton);
+        let sections = $('section').toArray();
+        $.each(sections, function() {
+            let sectionId = $(this).attr('id');
+            if (navButton == sectionId ) {
+                $(this).show("slide", 800);
+            }
+        }) 
+    }
+    
+    $('.nav-btn').click(function() {
+        let navButton = $(this).attr('name');
+        let sections = $('section').toArray();
+        $.each(sections, function() {
+            let sectionId = $(this).attr('id');
+            if (navButton != sectionId) {
+                $(this).hide("blind", 600);
+            }
+        }) 
+       
+        setTimeout(function() {
+            showSection(navButton);
+        }, 700);
+    })
+
+    //function for the mobile nav buttons
+    function showSection(navMobile) {
+        console.log(navMobile);
+        let sections = $('section').toArray();
+        $.each(sections, function() {
+            let sectionId = $(this).attr('id');
+            if (navMobile == sectionId ) {
+                $(this).show("blind", 800);
+            }
+        }) 
+    }
+    
+    $('.nav-btn-mobile').click(function() {
+        let navMobile = $(this).attr('name');
+        let sections = $('section').toArray();
+        $.each(sections, function() {
+            let sectionId = $(this).attr('id');
+            if (navMobile != sectionId) {
+                $(this).hide("clip", 600);
+            }
+        }) 
+       
+        setTimeout(function() {
+            showSection(navMobile);
+        }, 700);
+    })
 })
+
